76fcb852dda189ac897e0ea73571787269e54105
I m r a n <imurokks@gmail.com>
2023-12-10 19:08:21 +0530

    init

---
+++ src/main/java/org/zeta/devproductivity/DevproductivityApplication.java
@@ -0,0 +1,13 @@
+package org.zeta.devproductivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevproductivityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevproductivityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zeta/devproductivity/DevproductivityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zeta.devproductivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevproductivityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
